9970d6a3a6537b3cbd549500e91461894b67a010
avshalomh <avshalomh@users.noreply.github.com>
2017-07-14 16:05:13 +0300

    Update socketManager.js

--- src/services/socketManager.js
+++ src/services/socketManager.js
@@ -1,6 +1,6 @@
-import io from 'socket.io/node_modules/socket.io-client';
-// was - import io from 'socket.io-client/socket.io';
-// t
+
+// import io from 'socket.io/node_modules/socket.io-client';
+import io from 'socket.io-client/socket.io';
 
 const socketIo = io.connect(window.location.host, {reconnect: true});
 
