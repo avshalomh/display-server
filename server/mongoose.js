8aa6f1f2d746bc726f03bfa761bee4070ce574f6
Avshalom Houri <avshalom.houri@gmail.com>
2020-12-13 20:07:16 +0200

    mongoose

--- server/mongoose.js
+++ server/mongoose.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 const config = require('./config');
 mongoose.Promise = global.Promise;
 
-mongoose.set('useNewUrlParser', true);
-//  mongoose.set('useFindAndModify', false);
-mongoose.set('createIndexes', true);
-mongoose.connect(config.mongo.url, { }).then(() => {
+mongoose.connect(config.mongo.url, { useNewUrlParser: true }).then(() => {
   console.log('Connected to mongoDB');
 });
 
