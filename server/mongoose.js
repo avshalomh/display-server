988d40434c5edb05f84f465393f5b928cc0e2a73
Avshalom Houri <avshalom.houri@gmail.com>
2020-12-13 20:00:23 +0200

    mongoose

--- server/mongoose.js
+++ server/mongoose.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise;
 
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
+mongoose.set('createIndexes', true);
 mongoose.connect(config.mongo.url, { }).then(() => {
   console.log('Connected to mongoDB');
 });
