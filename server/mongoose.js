4bf22d4a848029e64bb088bac71a1ae5a45db8c0
Avshalom Houri <avshalom.houri@gmail.com>
2020-12-13 19:44:31 +0200

    createIndexes: true

--- server/mongoose.js
+++ server/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const config = require('./config');
 mongoose.Promise = global.Promise;
 
-mongoose.connect(config.mongo.url, { useUnifiedTopology: true,  CreateIndexes: true, useNewUrlParser: true }).then(() => {
+mongoose.connect(config.mongo.url, { useUnifiedTopology: true,  createIndexes: true, useNewUrlParser: true }).then(() => {
   console.log('Connected to mongoDB');
 });
 
