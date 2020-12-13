6292f69ab0b30c44241e3a872c616360c54e49c0
Avshalom Houri <avshalom.houri@gmail.com>
2020-12-13 19:11:04 +0200

    useMongoClient: true

--- server/mongoose.js
+++ server/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const config = require('./config');
 mongoose.Promise = global.Promise;
 
-mongoose.connect(config.mongo.url, { useNewUrlParser: true }).then(() => {
+mongoose.connect(config.mongo.url, { useMongoClient: true, useNewUrlParser: true }).then(() => {
   console.log('Connected to mongoDB');
 });
 
